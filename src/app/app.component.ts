735a69830a2bf84a6411b3a9de9bfbae03dc4b0b
Amrutha hebbar <hebbaramrutha@gmail.com>
2018-06-06 16:42:02 +0530

    Added files
    Added files

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles:[
+    `
+    .list-group-item:first-child
+    {
+      border-top-left-radius:0;
+      border-top-right-radius:0;
+      border-top:0;
+    }
+    `
+  ]
+
+})
+export class AppComponent implements OnInit {
+  query :string;
+  artists:object;
+
+  showArtist(item)
+  {
+    this.query=item.name;
+    item.highlight=!item.highlight;
+  }
+
+  constructor(private http:HttpClient) {
+    this.artists=[];
+}
+
+ngOnInit() :void
+{
+  this.http.get<Object>('../assets/data.json').subscribe(
+    data=>{
+      this.artists=data;
+    }
+  )
+}
+}
